9a01ac68510b0cca96211688d72eaddacd5bec4a
Tanvir Ahmed Sojal <jonglisojal@gmail.com>
2019-06-17 18:52:26 +0600

    Delete TeacherDatabaseOperation.java

--- src/users/TeacherDatabaseOperation.java
+++
@@ -1,9 +0,0 @@
-package users;
-
-import javafx.collections.ObservableList;
-
-import java.sql.SQLException;
-
-public interface TeacherDatabaseOperation {
-    ObservableList <Teacher> getAllTeachers() throws SQLException;
-}
